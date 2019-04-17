e1d45a277162a21a1413aa5515d91fbbfa06dde8
glennsaitch <49713027+glennsaitch@users.noreply.github.com>
2019-04-17 09:57:30 +0100

    Maybe?
    I don't know.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=whereinwesterosbot-Web-Deploy.pubxml -p:Password=txz7NBGEwAH7dcSnyHSTzkkB3zZQzR9qoFNoZFTTXEviTu5xKPhkMw6M8Gkk
+
